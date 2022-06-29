1e6e653e3a64df7f9e5f5265471329621c9ef9a9
No Name <87372653+PrajyotDaphal30@users.noreply.github.com>
2022-06-29 20:59:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from speak import *
+from SpeechRecognition import *
+from Temperature import *
+
+if __name__ == "__main__":
+    startup()
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+     # Logic for executing tasks based on query
+        if 'temperature' in query:
+            Temp()       
